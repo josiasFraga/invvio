584de980e8ee9eb46055747db127881b9ffc9bdc
Josias Fraga <josiasrs2009@gmail.com>
2025-09-03 16:17:02 -0300

    em andamento

--- src/constants/configs.js
+++ src/constants/configs.js
@@ -4,7 +4,7 @@ export default {
 	dimensions: Dimensions.get('window'),
 	platform: Platform.OS,
 
-	url: 'http://192.168.1.21:3000/api',
+	url: 'http://192.168.1.10:3000/api',
     
 	STATUSBAR_HEIGHT: Platform.OS === 'ios' ? 45 : StatusBar.currentHeight,
 	ORIGINAL_HEADER_HEIGHT_WITHOUT_STATUS_BAR: 70
